31be8865c4b6e096a2ba91523d3a344cde5a546d
jarbasal <jarbasai@mailfence.com>
2020-12-23 11:08:03 +0000

    skills config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ovos_utils',
-    version='0.0.3',
+    version='0.0.4',
     packages=['ovos_utils',
               'ovos_utils.sound',
               'ovos_utils.mark1',
