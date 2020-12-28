75847d3d9cdaeff8a8335a1aa6ee7b5bc5c67215
jarbasal <jarbasai@mailfence.com>
2020-12-28 12:19:36 +0000

    0.0.4c2 - circular imports

--- ovos_utils/enclosure/__init__.py
+++ ovos_utils/enclosure/__init__.py
@@ -1,6 +1,6 @@
-from ovos_utils.system import get_platform_fingerprint, MycroftRootLocations
-
+from ovos_utils.system import MycroftRootLocations
 from enum import Enum
+from os.path import exists
 
 
 class MycroftEnclosures(str, Enum):
@@ -31,20 +31,26 @@ def enclosure2rootdir(enclosure=None):
 
 
 def detect_enclosure():
+    # TODO avoid circular import better
+    # this is the only safe import to use from ovos_utils.configuration,
+    # other methods are NOT, because they depend on this method directly,
+    # ie, configuration objects use detect_enclosure
+    from ovos_utils.configuration import MycroftSystemConfig
+
     # TODO very naive check, improve this
     # use db of reference fingerprints
-    fingerprint = get_platform_fingerprint()
-    if fingerprint["enclosure"] == "OpenVoiceOS":
+    fingerprint = MycroftSystemConfig().get("enclosure", {}).get("platform",
+                                                                 "unknown")
+    if fingerprint == "OpenVoiceOS":
         return MycroftEnclosures.OVOS
-    elif fingerprint["enclosure"] == "mycroft_mark_1":
-        if fingerprint["mycroft_core_location"] == MycroftRootLocations.MARK1 or \
-                fingerprint["mycroft_core_location"] == MycroftRootLocations.OLD_MARK1:
+    elif fingerprint == "mycroft_mark_1":
+        if exists(MycroftRootLocations.OLD_MARK1):
             return MycroftEnclosures.OLD_MARK1
         return MycroftEnclosures.MARK1
-    elif fingerprint["enclosure"] == "mycroft_mark_2":
+    elif fingerprint == "mycroft_mark_2":
         # TODO bigscreen also reports this...
         return MycroftEnclosures.MARK2
-    elif fingerprint["enclosure"] == "picroft":
+    elif fingerprint == "picroft":
         return MycroftEnclosures.PICROFT
 
-    return "unknown"
+    return MycroftEnclosures.OTHER
